c55c81f0ca8837d02294284bdfe99953a710f7f0
alexanderpetrov <alexander.petrov@novarto.com>
2017-02-17 21:28:56 +0200

    users search and logic

--- public/scripts/widgets/startmenu/startmenu.js
+++ public/scripts/widgets/startmenu/startmenu.js
@@ -2,42 +2,19 @@ Backbone.widget({
 
     model: {},
     template: false,
-    columnCount: 3,
-
-
 
     events: {
-
-        'click .user' : 'selectUser',
 
 
     },
 
     loaded: function () {
-        this.loadUsers()
+
     },
 
-    loadUsers: function(){
-        this.ajaxRequest({
-            url: 'webservices/users.json',
-            data: {},
-            type: "GET",
-            success: function (response) {
-                this.model.users = response;
-                this.render();
-            }
-        });
-    },
 
     render: function(e){
-        this.renderTemplate({
 
-            template: 'users',
-            data: this.model,
-            renderCallback: function () {
-               console.log('rendered')
-            }
-        })
     },
 
     selectUser: function(){
